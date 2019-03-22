59f1683844f9889fb106f82ad8dbe267db3c08c8
macjabeth <macbethjonathan@gmail.com>
2019-03-22 19:02:45 +0000

    add in styled components

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,56 @@ import axios from 'axios';
 
 const url = 'http://penguin.linux.test:4000/api/projects';
 
+// STYLES
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #111;
+    color: #fff;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1 {
+    color: #e8e;
+    text-transform: uppercase;
+    padding-bottom: 5px;
+    border-bottom: 1px solid;
+    display: inline-block;
+    margin-bottom: .25em;
+  }
+
+  a {
+    color: #ebe;
+    cursor: pointer;
+  }
+
+  details {
+    margin-left: 30px;
+    margin-bottom: 15px;
+  }
+
+  summary {
+    outline: none;
+    margin-bottom: 5px;
+  }
+`;
+
+const AppContainer = styled.div`
+  padding: 15px;
+`;
+
+const LinkList = styled.ul`
+  a.active {
+    color: red;
+  }
+`;
+
+// COMPONENTS
 class App extends Component {
   state = { projects: [] }
 
@@ -21,29 +71,32 @@ class App extends Component {
     const { projects } = this.state;
     const { location } = this.props;
     return (
-      <div className="app">
-        <h1>Web API</h1>
-        <h5>Welcome... Please continue by clicking the link below. It will show you list of projects.</h5>
-        {location.pathname === '/' ? (
-          <Link to="/api/projects">Display Projects</Link>
-        ) : (
-          <Link to="/">Hide Projects</Link>
-        )}
-        <Route path="/api/projects" render={props => (
-          <React.Fragment>
-            <h3>Projects</h3>
-            <ul>
-              {projects.map(project => (
-                <li key={project.id}>
-                  <NavLink to={`${props.match.url}/${project.id}`}>{project.name}</NavLink>
-                  <p>{project.description}</p>
-                </li>
-              ))}
-            </ul>
-          </React.Fragment>
-        )} />
-        <Route path="/api/projects/:id" component={Actions} />
-      </div>
+      <React.Fragment>
+        <GlobalStyles />
+        <AppContainer>
+          <h1>Web API</h1>
+          <h5>Welcome... Please continue by clicking the link below. It will show you list of projects.</h5>
+          {location.pathname === '/' ? (
+            <Link to="/api/projects">Display Projects</Link>
+          ) : (
+              <Link to="/">Hide Projects</Link>
+            )}
+          <Route path="/api/projects" render={props => (
+            <React.Fragment>
+              <h3>Projects</h3>
+              <LinkList>
+                {projects.map(project => (
+                  <li key={project.id}>
+                    <NavLink to={`${props.match.url}/${project.id}`}>{project.name}</NavLink>
+                    <p>{project.description}</p>
+                  </li>
+                ))}
+              </LinkList>
+            </React.Fragment>
+          )} />
+          <Route path="/api/projects/:id" component={Actions} />
+        </AppContainer>
+      </React.Fragment>
     );
   }
 }
@@ -81,8 +134,8 @@ class Actions extends Component {
             ))}
           </React.Fragment>
         ) : (
-          <p>Loading...</p>
-        )}
+            <p>Loading...</p>
+          )}
       </div>
     );
   }
